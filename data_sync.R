137a0dd12f43b652ca9962f450ea297af26ecc81
meghasyam <itismeghasyam@mail.com>
2023-04-19 18:12:46 +0000

    added parameters section and comments

--- data_sync.R
+++ data_sync.R
@@ -4,11 +4,29 @@
 ##############
 source('awscli_utils.R')
 
-## sync S3 buckets 
-# <<values below are test values>>
-s3SyncBuckets(source_bucket = 's3://sc-237179673806-pp-hrx2giywqix2s-s3bucket-mhb2u0tf0ift/',
-              destination_bucket = 's3://sc-237179673806-pp-daamam3ykuje4-s3bucket-1jslzxl1xq4zm/')
+#############
+# Parameters
+#############
+SOURCE_BUCKET = 'sc-237179673806-pp-hrx2giywqix2s-s3bucket-mhb2u0tf0ift'
+# S3 bucket whose objects are being copied
 
-## sync the pre-ETL bucket to local EC2 instance
+DESTINATION_BUCKET = 'sc-237179673806-pp-daamam3ykuje4-s3bucket-1jslzxl1xq4zm'
+# S3 bucket to where objects are being copied into
+
+LOCAL_SYNC_BUCKET = 'sc-237179673806-pp-i7vklp56the66-s3bucket-eal8qlgc87kj'
+# S3 bucket to be synced locally
+    
+AWS_DOWNLOAD_LOCATION = 'temp_aws_2'
+# Local location where Source bucket files are synced to
+
+#############
+# Sync S3 buckets 
+#############
 # <<values below are test values>>
-s3SyncToLocal(source_bucket = 's3://sc-237179673806-pp-daamam3ykuje4-s3bucket-1jslzxl1xq4zm/')
+s3SyncBuckets(source_bucket = paste0('s3://', SOURCE_BUCKET,'/'),
+              destination_bucket = paste0('s3://', DESTINATION_BUCKET,'/'))
+#############
+# Sync the pre-ETL bucket to local EC2 instance
+#############
+# <<values below are test values>>
+s3SyncToLocal(source_bucket = paste0('s3://', LOCAL_SYNC_BUCKET,'/'), local_destination = AWS_DOWNLOAD_LOCATION)
--- synIndex.R
+++ synIndex.R
@@ -1,48 +1,78 @@
+################################
+## Index S3 Objects in Synapse
+################################
+
 #############
-# Index S3 Objects in Synapse
+# Required functions and libraries
 #############
 library(tidyverse)
 library(synapser)
 library(rjson)
 synapser::synLogin()
-
-### First run data_sync.R and sync the S3 bucket to the local EC2 instance
-
 source('awscli_utils.R')
 
+
+#############
+# Parameters
+#############
+FILE_LIST_OUTPUT = 's3files.txt' 
+# file name of the file where the aws ls output is saved
+
+SOURCE_BUCKET = 'sc-237179673806-pp-i7vklp56the66-s3bucket-eal8qlgc87kj'
+# S3 bucket whose objects are being listed
+
+SYNAPSE_PARENT_ID = 'syn51364747'
+# Synapse location where the objects are listed
+
+AWS_DOWNLOAD_LOCATION = 'temp_aws_2'
+# Local location where Source bucket files are synced to
+
+
+#############
+# NOTE
+#############
+### First run data_sync.R and sync the S3 bucket to the local EC2 instance
+
+
+#############
+# Get bucket params and file list
+#############
 ## Get a list of all Objects in the S3 bucket
-s3lsBucketObjects(source_bucket = 's3://sc-237179673806-pp-daamam3ykuje4-s3bucket-1jslzxl1xq4zm/',
-                  output_file = 's3files.txt')
+s3lsBucketObjects(source_bucket = paste0('s3://', SOURCE_BUCKET,'/'),
+                  output_file = FILE_LIST_OUTPUT)
 
 # Get bucket params
 bucket_params <- list(uploadType='S3',
                     concreteType='org.sagebionetworks.repo.model.project.ExternalS3StorageLocationSetting',
-                    bucket='sc-237179673806-pp-daamam3ykuje4-s3bucket-1jslzxl1xq4zm')
+                    bucket=SOURCE_BUCKET)
 
 # The above list is just to verify/reference, 
 # since we will replicate the structure locally
 # we will work with that
 
 ## Get a list of all local files
-localFileList <- list.files(path = 'temp_aws',
+localFileList <- list.files(path = AWS_DOWNLOAD_LOCATION,
                             all.files = TRUE, # get hidden files too
                             recursive = TRUE, # get files inside sub-folders (if any)
                             full.names = FALSE) # to get the directory path prepended to the file name
 
+#############
+# Index in Synapse
+#############
 ## For each file index it in Synapse given a parent synapse folder
 for(file_ in localFileList){
   print(file_)
-  absolute_file_path <- tools::file_path_as_absolute(paste0('temp_aws/',file_))
+  absolute_file_path <- tools::file_path_as_absolute(paste0(AWS_DOWNLOAD_LOCATION,'/',file_))
   
   temp_syn_obj <- synapser::synCreateExternalS3FileHandle(
     bucket_name = bucket_params$bucket,
     s3_file_key = file_, # 
     file_path = absolute_file_path,
-    parent = 'syn51273216'
+    parent = SYNAPSE_PARENT_ID
   )
   
   f <- File(dataFileHandleId=temp_syn_obj$id, 
-            parentId='syn51273216',
+            parentId=SYNAPSE_PARENT_ID,
             name = temp_syn_obj$fileName) ## set file name same as the one from realpath
   
   f <- synStore(f)
