a8b2dfa0d3bbd9cedbcbfe18b27d3adb5a9629d2
meghasyam <itismeghasyam@mail.com>
2023-05-05 17:12:24 +0000

    Moved directory selection and deletion code to data_sync from synIndex

--- synIndex.R
+++ synIndex.R
@@ -25,63 +25,6 @@ source('awscli_utils.R')
 #############
 source('params.R')
 
-#############
-# NOTE
-#############
-### First run data_sync.R and sync the S3 bucket to the local EC2 instance
-
-#############
-# Get bucket params and file list
-#############
-## Get a list of all Objects in the PRE_ETL S3 bucket 
-# s3lsBucketObjects(source_bucket = paste0('s3://', PRE_ETL_BUCKET,'/'),
-#                   output_file = FILE_LIST_OUTPUT)
-
-# Get bucket params
-bucket_params <- list(uploadType='S3',
-                      concreteType='org.sagebionetworks.repo.model.project.ExternalS3StorageLocationSetting',
-                      bucket=PRE_ETL_BUCKET)
-
-# The above list is just to verify/reference, 
-# since we will replicate the structure locally
-# we will work with that
-
-# all folders inside the AWS_DOWNLOAD_LOCATION
-# When we create dataFileHandleId in synapse it will create some folders in the source S3 bucket
-# we don't need those
-localDirs <- list.dirs(path = AWS_DOWNLOAD_LOCATION, full.names = FALSE) 
-
-## The folders we want to show in synapse
-## This would depend on the how the data is organized in the main S3 INGRESS bucket
-FOLDERS_TO_SYNC_SYNAPSE <- c('adults',
-                             'pregnant',
-                             'pediatric') 
-
-## From the local AWS location remove all folders that are not the ones selected above
-dirs_to_delete <- dplyr::setdiff(localDirs, c(FOLDERS_TO_SYNC_SYNAPSE, "")) # "" is the local directory, a result of list.dirs(.. full.names=FALSE)
-
-## delete the unwanted directories before creating a upload manifest
-for(dir_ in dirs_to_delete){
-  unlink(paste0(AWS_DOWNLOAD_LOCATION,"/",dir_), recursive = TRUE)
-}
-
-#############
-# Get a manifest of files to upload
-#############
-## The S3 bucket is synced to AWS_DOWNLOAD_LOCATION locally. 
-## We will use synapse cmd line client manifest function to replicate the folder structure 
-## but NOT upload the files. We will create a datafilehandleid in place later instead of uploading file
-
-old_path <- Sys.getenv("PATH")
-Sys.setenv(PATH = paste(old_path, "/home/ubuntu/R/x86_64-pc-linux-gnu-library/3.6/PythonEmbedInR/bin", sep = ":"))
-## When we install synapser, it installs synapseclient and along with it the synapse cmd line client
-## We need to add the location of synapseclient to the system path so that it can recognize synapse cmd 
-
-SYSTEM_COMMAND <- glue::glue('synapse -u "{SYNAPSE_USERNAME}" -p "{SYNAPSE_PASSWORD}" manifest --parent-id {SYNAPSE_PARENT_ID} --manifest current_manifest.tsv {AWS_DOWNLOAD_LOCATION}')
-
-## Generate manifest file
-system(SYSTEM_COMMAND)
-
 ###########
 ## Get a list of all files to upload and their synapse locations(parentId) 
 ###########
@@ -119,7 +62,7 @@ if(nrow(synapse_manifest_to_upload) > 0){ # there are some files to upload
     absolute_file_path <- tools::file_path_as_absolute(file_) # local absolute path
     
     temp_syn_obj <- synapser::synCreateExternalS3FileHandle(
-      bucket_name = bucket_params$bucket,
+      bucket_name = PRE_ETL_BUCKET,
       s3_file_key = s3_file_key, #
       file_path = absolute_file_path,
       parent = parent_id
