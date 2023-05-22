a17326b029b366861e639b4b0297448cf8e6e504
pranavanba <pranavanba@gmail.com>
2023-05-22 11:51:21 -0700

    Update bucket identifiers

--- params.R
+++ params.R
@@ -2,10 +2,12 @@
 ## Required Parameters
 ################################
 
-INGRESS_BUCKET = 'sc-237179673806-pp-a2c32zmbbq566-s3bucket-2vq5unuhcxjo'
+# INGRESS_BUCKET = 'sc-237179673806-pp-a2c32zmbbq566-s3bucket-2vq5unuhcxjo'
+INGRESS_BUCKET = 'recover-dev-ingestion'
 # S3 bucket whose objects are being copied
 
-PRE_ETL_BUCKET = 'sc-237179673806-pp-lytud6mnzjczm-s3bucket-edpunhjxp7bs'
+# PRE_ETL_BUCKET = 'sc-237179673806-pp-lytud6mnzjczm-s3bucket-edpunhjxp7bs'
+PRE_ETL_BUCKET = 'recover-dev-input-data'
 # S3 bucket to where objects are being copied into
 
 AWS_DOWNLOAD_LOCATION = './temp_aws/staging/'
@@ -14,7 +16,7 @@ AWS_DOWNLOAD_LOCATION = './temp_aws/staging/'
 FILE_LIST_OUTPUT = 's3files.txt' 
 # file name of the file where the aws ls output is saved
 
-SYNAPSE_PARENT_ID = 'syn51399136'
+SYNAPSE_PARENT_ID = 'syn51517390'
 # Synapse location where the S3 bucket objects are listed
 
 SYNAPSE_FILEVIEW_ID = 'syn51399596'
