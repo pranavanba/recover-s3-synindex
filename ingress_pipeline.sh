eef31f379144c2c03b8e0900efffd8827a1e5b56
pranavanba <pranavanba@gmail.com>
2023-05-22 11:51:10 -0700

    Update file paths

--- ingress_pipeline.sh
+++ ingress_pipeline.sh
@@ -1,3 +1,3 @@
-Rscript /home/ubuntu/recover-s3-synindex/data_sync.R
-Rscript /home/ubuntu/recover-s3-synindex/generate_manifest.R
-Rscript /home/ubuntu/recover-s3-synindex/synIndex.R
+Rscript /recover-s3-synindex/data_sync.R
+Rscript /recover-s3-synindex/generate_manifest.R
+Rscript /recover-s3-synindex/synIndex.R
