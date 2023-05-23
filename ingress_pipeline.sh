eb32d649e34349727eba7157ec056975a2f79d85
pranavanba <pranavanba@gmail.com>
2023-05-23 15:56:28 -0700

    Modify commands for testing

--- ingress_pipeline.sh
+++ ingress_pipeline.sh
@@ -1,4 +1,3 @@
-cat /.aws
-# Rscript /recover-s3-synindex/data_sync.R
-# Rscript /recover-s3-synindex/generate_manifest.R
-# Rscript /recover-s3-synindex/synIndex.R
+Rscript /recover-s3-synindex/data_sync.R
+Rscript /recover-s3-synindex/generate_manifest.R
+Rscript /recover-s3-synindex/synIndex.R
