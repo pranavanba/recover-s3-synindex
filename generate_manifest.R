d8681eaa25e18b0d1feff0233ec7083b16e933b1
pranavanba <pranavanba@gmail.com>
2023-05-25 15:35:47 -0700

    Comment out references to synapseclient install path

--- generate_manifest.R
+++ generate_manifest.R
@@ -6,7 +6,7 @@
 ### Synapse credentials and params
 #############
 # Set the environment variables .Renviron file in your home folder. Refer to README for more details
-SYNAPSECLIENT_INSTALL_PATH = Sys.getenv('SYNAPSECLIENT_INSTALL_PATH')
+# SYNAPSECLIENT_INSTALL_PATH = Sys.getenv('SYNAPSECLIENT_INSTALL_PATH')
 source('/recover-s3-synindex/params.R')
 ### First run data_sync.R
 
@@ -14,13 +14,13 @@ source('/recover-s3-synindex/params.R')
 ## We will use synapse cmd line client manifest function to replicate the folder structure 
 ## but NOT upload the files. We will create a datafilehandleid in place later instead of uploading file
 
-old_path <- Sys.getenv("PATH")
+# old_path <- Sys.getenv("PATH")
 
-if(!grepl(SYNAPSECLIENT_INSTALL_PATH,old_path)){
-  Sys.setenv(PATH = paste(old_path, SYNAPSECLIENT_INSTALL_PATH, sep = ":"))
-  ## When we install synapser, it installs synapseclient and along with it the synapse cmd line client
-  ## We need to add the location of synapseclient to the system path so that it can recognize synapse cmd 
-}
+# if(!grepl(SYNAPSECLIENT_INSTALL_PATH,old_path)){
+#   Sys.setenv(PATH = paste(old_path, SYNAPSECLIENT_INSTALL_PATH, sep = ":"))
+#   ## When we install synapser, it installs synapseclient and along with it the synapse cmd line client
+#   ## We need to add the location of synapseclient to the system path so that it can recognize synapse cmd 
+# }
 
 SYSTEM_COMMAND <- glue::glue('synapse manifest --parent-id {SYNAPSE_PARENT_ID} --manifest ./current_manifest.tsv {AWS_DOWNLOAD_LOCATION}')
 
