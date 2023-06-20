48545dcd1d66365aa56628c89346742323ea052b
Ubuntu <ubuntu@ip-10-41-28-149.ec2.internal>
2023-06-20 16:54:30 +0000

    _ ability to select different profiles in aws commands

--- awscli_utils.R
+++ awscli_utils.R
@@ -6,8 +6,8 @@
 ####
 # Sync between two buckets - source and destination
 ####
-s3SyncBuckets <- function(source_bucket, destination_bucket){
-  command_in <- glue::glue('aws --profile service-catalog s3 sync {source_bucket} {destination_bucket}')
+s3SyncBuckets <- function(source_bucket, destination_bucket, aws_profile='service-catalog'){
+  command_in <- glue::glue('aws --profile {aws_profile} s3 sync {source_bucket} {destination_bucket}')
   print(paste0('RUNNING: ',command_in))
   system(command_in)
 }
@@ -15,8 +15,8 @@ s3SyncBuckets <- function(source_bucket, destination_bucket){
 ####
 # Sync to local location from a source bucket
 ####
-s3SyncToLocal <- function(source_bucket, local_destination = './temp_aws'){
-  command_in <- glue::glue('aws --profile service-catalog s3 sync {source_bucket} {local_destination}')
+s3SyncToLocal <- function(source_bucket, local_destination = './temp_aws', aws_profile='service-catalog'){
+  command_in <- glue::glue('aws --profile {aws_profile} s3 sync {source_bucket} {local_destination}')
   print(paste0('RUNNING: ',command_in))
   system(command_in)
 }
@@ -24,8 +24,8 @@ s3SyncToLocal <- function(source_bucket, local_destination = './temp_aws'){
 ####
 # Get a list of all files in a bucket, Stores to a txt file in the local working directory
 ####
-s3lsBucketObjects <- function(source_bucket, output_file='s3files.txt'){
-  command_in <- glue::glue('aws --profile service-catalog s3 ls {source_bucket} --recursive --output text > {output_file}')
+s3lsBucketObjects <- function(source_bucket, output_file='s3files.txt', aws_profile='service-catalog'){
+  command_in <- glue::glue('aws --profile {aws_profile} s3 ls {source_bucket} --recursive --output text > {output_file}')
   print(paste0('RUNNING: ',command_in))
   system(command_in)
 }
