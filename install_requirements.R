d8e24607ed15de4bf59c3a502a1fc8df0f8a528f
pranavanba <pranavanba@gmail.com>
2023-06-21 10:57:51 -0700

    Modify list of packages installed

--- install_requirements.R
+++ install_requirements.R
@@ -1,6 +1,12 @@
 #############
 # Required libraries to install
 #############
-install.packages('tidyverse')
+install.packages("magrittr")
+install.packages("glue")
+install.packages("dplyr")
+install.packages("rjson")
+install.packages("stringr")
+install.packages("tools")
+install.packages("reticulate")
 install.packages("synapser", repos=c("http://ran.synapse.org", "http://cran.fhcrc.org"))
 install.packages("synapserutils", repos=c("http://ran.synapse.org", "http://cran.fhcrc.org"))
