ab52cb759e2e948e6cd6ce1316bb2537738c38c0
Ubuntu <ubuntu@ip-10-41-28-149.ec2.internal>
2023-07-05 19:55:03 +0000

    changed aws utils functions to use env variables too

--- awscli_utils.R
+++ awscli_utils.R
@@ -17,6 +17,23 @@ s3SyncBuckets <- function(source_bucket, destination_bucket, aws_profile='servic
 ####
 s3SyncToLocal <- function(source_bucket, local_destination = './temp_aws', aws_profile='service-catalog'){
   command_in <- glue::glue('aws --profile {aws_profile} s3 sync {source_bucket} {local_destination}')
+  if(aws_profile == 'env-var'){
+    command_in <- glue::glue('aws s3 sync {source_bucket} {local_destination}')
+  }
+  
+  print(paste0('RUNNING: ',command_in))
+  system(command_in)
+}
+
+####
+# Sync from local location to a destination bucket
+####
+s3SyncFromLocal <- function(local_source = './temp_aws', destination_bucket, aws_profile='service-catalog'){
+  command_in <- glue::glue('aws --profile {aws_profile} s3 sync {local_source} {destination_bucket}')
+  if(aws_profile == 'env-var'){
+    command_in <- glue::glue('aws s3 sync {local_source} {destination_bucket}')
+  }
+  
   print(paste0('RUNNING: ',command_in))
   system(command_in)
 }
@@ -26,6 +43,10 @@ s3SyncToLocal <- function(source_bucket, local_destination = './temp_aws', aws_p
 ####
 s3lsBucketObjects <- function(source_bucket, output_file='s3files.txt', aws_profile='service-catalog'){
   command_in <- glue::glue('aws --profile {aws_profile} s3 ls {source_bucket} --recursive --output text > {output_file}')
+  if(aws_profile == 'env-var'){
+    command_in <- glue::glue('aws s3 ls {source_bucket} --recursive --output text > {output_file}')
+  }
+
   print(paste0('RUNNING: ',command_in))
   system(command_in)
 }
