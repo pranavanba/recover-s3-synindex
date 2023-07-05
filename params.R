8c71a8d19fe89287dab5afb4b448304cc3ade1bc
Ubuntu <ubuntu@ip-10-41-28-149.ec2.internal>
2023-07-05 19:53:47 +0000

    change to dev buckets

--- params.R
+++ params.R
@@ -2,14 +2,14 @@
 ## Required Parameters
 ################################
 
-INGRESS_BUCKET = 'sc-237179673806-pp-a2c32zmbbq566-s3bucket-2vq5unuhcxjo'
+INGRESS_BUCKET = 'recover-dev-ingestion'
 # S3 bucket whose objects are being copied
 
-PRE_ETL_BUCKET = 'sc-237179673806-pp-lytud6mnzjczm-s3bucket-edpunhjxp7bs'
+PRE_ETL_BUCKET = 'recover-dev-input-data'
 # S3 bucket to where objects are being copied into
 
 AWS_DOWNLOAD_LOCATION = './temp_aws/main/'
-# Local location where Source bucket files are synced to
+# Local location where Ingress bucket files are synced to
 
 FILE_LIST_OUTPUT = 's3files.txt' 
 # file name of the file where the aws ls output is saved
