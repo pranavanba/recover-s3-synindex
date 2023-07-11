1520745f9799bb1c65983c25531470faf70d07de
Ubuntu <ubuntu@ip-10-41-28-149.ec2.internal>
2023-07-11 21:51:53 +0000

    Changing to Prod params

--- params.R
+++ params.R
@@ -2,10 +2,10 @@
 ## Required Parameters
 ################################
 
-INGRESS_BUCKET = 'recover-dev-ingestion'
+INGRESS_BUCKET = 'recover-ingestion'
 # S3 bucket whose objects are being copied
 
-PRE_ETL_BUCKET = 'recover-dev-input-data'
+PRE_ETL_BUCKET = 'recover-input-data'
 # S3 bucket to where objects are being copied into
 
 AWS_DOWNLOAD_LOCATION = './temp_aws/main/'
@@ -14,8 +14,8 @@ AWS_DOWNLOAD_LOCATION = './temp_aws/main/'
 FILE_LIST_OUTPUT = 's3files.txt' 
 # file name of the file where the aws ls output is saved
 
-SYNAPSE_PARENT_ID = 'syn51399136'
+SYNAPSE_PARENT_ID = 'syn51714264'
 # Synapse location where the S3 bucket objects are listed
 
-SYNAPSE_FILEVIEW_ID = 'syn51399596'
+SYNAPSE_FILEVIEW_ID = 'syn51406705'
 # Synapse ID of the fileview containing list of all currentlty indexed S3 objects in Synapse
