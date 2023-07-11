b6dee099b36bafc052acc9285e13e8080a65470d
Ubuntu <ubuntu@ip-10-41-28-149.ec2.internal>
2023-07-11 23:27:49 +0000

    Renaming folders to contain underscore '_' instead of backslash '\'. i.e 'adults\v1` to 'adults_v1'

--- data_sync.R
+++ data_sync.R
@@ -18,6 +18,26 @@ s3SyncToLocal(source_bucket = paste0('s3://', INGRESS_BUCKET,'/'),
               local_destination = AWS_DOWNLOAD_LOCATION,
               aws_profile = 's3-external-collab') 
 
+
+#############
+# Rename folders with '\' to have '_' for eg., 'adults\v1' to 'adults_v1'
+#############
+# We will work with the locally replicated structure 
+# all folders inside the AWS_DOWNLOAD_LOCATION, i.e all folders at AWS_DOWNLOAD_LOCATION/
+localDirs <- list.dirs(path = AWS_DOWNLOAD_LOCATION, full.names = FALSE, recursive = FALSE) 
+
+# The folders are named as 'adults\v1', 'pregnant\v1' and 'pediatric\v1'. We want to remove the
+# '\' and replace it with '_'
+for(dir_ in localDirs){
+  dir_newName <- stringr::str_replace_all(dir_, "\\\\",'_')
+  # rename each folder
+  file.rename(paste0(AWS_DOWNLOAD_LOCATION,"/",dir_), paste0(AWS_DOWNLOAD_LOCATION,"/",dir_newName))
+}
+
+# Get newly renamed folders
+localDirs <- list.dirs(path = AWS_DOWNLOAD_LOCATION, full.names = FALSE, recursive = FALSE) 
+
+
 #############
 # Sync Local EC2 (from ingress bucket) to pre_etl bucket (Second Sync )
 #############
@@ -48,15 +68,11 @@ s3SyncFromLocal(local_source = AWS_DOWNLOAD_LOCATION,
 #############
 # Get bucket params and file list
 #############
-# We will work with the locally replicated structure 
-# all folders inside the AWS_DOWNLOAD_LOCATION, i.e all folders at AWS_DOWNLOAD_LOCATION/
-localDirs <- list.dirs(path = AWS_DOWNLOAD_LOCATION, full.names = FALSE, recursive = FALSE) 
-
 ## The folders we want to show in synapse
 ## This would depend on the how the data is organized in the main S3 INGRESS bucket
-FOLDERS_TO_SYNC_SYNAPSE <- c('adults\\v1',
-                             'pregnant\\v1',
-                             'pediatric\\v1') 
+FOLDERS_TO_SYNC_SYNAPSE <- c('adults_v1',
+                             'pregnant_v1',
+                             'pediatric_v1') 
 
 # When we create dataFileHandleId in synapse it will create some folders in the source S3 bucket
 # we don't need those
